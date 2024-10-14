6d28f0cf40e9f817defe424ed3646a9f696d4c3e
Akihisa Yagi <caphosra@gmail.com>
2024-10-14 14:51:00 +0900

    Add Nix configuration files

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        hsPkgs = pkgs.haskell.packages."ghc982";
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            stack
+            hsPkgs.ghc
+            hsPkgs.haskell-language-server
+          ];
+        };
+      }
+    );
+}
