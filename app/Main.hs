30c204f6476183f208a325f994be84d25164d304
Akihisa Yagi <caphosra@gmail.com>
2024-10-17 23:15:12 +0900

    Add a simple argument parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,58 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main (main) where
 
-import Lib
+import Options.Applicative
+
+data CommandLineArg = CommandLineArg
+    { input :: [String]
+    , output :: String
+    , verbose :: Bool
+    , optimize :: Int
+    }
+
+-- | Parse a command line argument
+parseArg :: Parser CommandLineArg
+parseArg =
+    CommandLineArg
+        <$> many
+            ( strOption
+                ( long "input"
+                    <> short 'i'
+                    <> help "Input files"
+                    <> metavar "FILE"
+                )
+            )
+        <*> strOption
+            ( long "output"
+                <> short 'o'
+                <> help "An output file"
+                <> metavar "OUTFILE"
+            )
+        <*> switch
+            ( long "verbose"
+                <> short 'v'
+                <> help "Whether to be verbose"
+            )
+        <*> option
+            auto
+            ( long "optimize"
+                <> help "Optimization level"
+                <> showDefault
+                <> value 1000
+                <> metavar "INT"
+            )
 
 main :: IO ()
-main = someFunc
+main = execParser opts >>= execArgs
+  where
+    opts =
+        info
+            (parseArg <**> helper)
+            ( fullDesc
+                <> progDesc "MinCaml ---[Mincc]---> RINANA"
+                <> header "mincc (c) 2024 Akihisa Yagi\n\n"
+            )
+
+execArgs :: CommandLineArg -> IO ()
+execArgs arg = print $ input arg
