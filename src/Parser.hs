c56e4dcf7a0aa920ce7658d023f706fe9cf27ccb
Akihisa Yagi <caphosra@gmail.com>
2024-11-06 12:07:38 +0900

    Refactor Parser.hs to fix #6

--- src/Parser.hs
+++ src/Parser.hs
@@ -2,6 +2,7 @@
 
 module Parser (lexeme, parseLiteral, parseIdent, parsePattern, parseSimpleExpr, parseExpr) where
 
+import Control.Monad (void)
 import Control.Monad.Combinators.Expr
 import Data.Char (isAlphaNum, isDigit)
 import Data.Text (Text, cons, unpack)
@@ -29,11 +30,11 @@ Consumes trailing white spaces.
 lexeme :: Parser a -> Parser a
 lexeme = L.lexeme spaced
 
-symbol :: Text -> Parser Text
-symbol = L.symbol spaced
+symbol :: Text -> Parser ()
+symbol = void . L.symbol spaced
 
-cSymbol :: Char -> Parser Char
-cSymbol = lexeme . char
+cSymbol :: Char -> Parser ()
+cSymbol = lexeme . void . char
 
 -- | An identifier parser
 parseIdent :: Parser RawIdent
@@ -81,56 +82,57 @@ parseLiteral =
                 )
 
     parseNum :: Parser Int
-    parseNum = parseNumStr >>= \num -> return (read num)
+    parseNum =
+        read <$> parseNumStr
 
     parseFloat :: Parser Float
     parseFloat =
-        try
-            ( parseNumStr >>= \num ->
-                char '.' >> many digitChar >>= \num2 ->
-                    option "" parseExp >>= \e ->
-                        return $ read $ (if null num2 then num else num ++ "." ++ num2) ++ e
-            )
+        try $
+            do
+                num <- parseNumStr
+                _ <- char '.'
+                num2 <- many digitChar
+                e <- option "" parseExp
+                pure $ read $ (if null num2 then num else num ++ "." ++ num2) ++ e
       where
         parseExp :: Parser String
         parseExp =
-            try
-                ( (char 'e' <|> char 'E')
-                    >> option "" (string "+" <|> string "-")
-                    >>= \sign ->
-                        many digitChar
-                            >>= \num -> return $ "e" ++ unpack sign ++ num
-                )
+            try $
+                do
+                    _ <- char 'e' <|> char 'E'
+                    sign <- option "" (string "+" <|> string "-")
+                    num <- many digitChar
+                    pure $ "e" ++ unpack sign ++ num
 
 parseRelationBinOp :: Parser (Bool, BinaryOp)
 parseRelationBinOp =
     lexeme
-        ( try (string "=" >> return (False, RelationOp Eq))
-            <|> try (string "<=" >> return (True, RelationOp Ge))
-            <|> try (string ">=" >> return (False, RelationOp Ge))
-            <|> try (string "<>" >> return (False, RelationOp Ne))
-            <|> try (string "<" >> return (False, RelationOp Lt))
-            <|> try (string ">" >> return (True, RelationOp Lt))
+        ( try (string "=" >> pure (False, RelationOp Eq))
+            <|> try (string "<=" >> pure (True, RelationOp Ge))
+            <|> try (string ">=" >> pure (False, RelationOp Ge))
+            <|> try (string "<>" >> pure (False, RelationOp Ne))
+            <|> try (string "<" >> pure (False, RelationOp Lt))
+            <|> try (string ">" >> pure (True, RelationOp Lt))
         )
         <?> "a relation binary operator"
 
 parseTermOp :: Parser BinaryOp
 parseTermOp =
     lexeme
-        ( try (string "+." >> return (FloatOp FAdd))
-            <|> try (string "-." >> return (FloatOp FSub))
-            <|> try (string "+" >> return (IntOp Add))
-            <|> try (string "-" >> return (IntOp Sub))
+        ( try (string "+." >> pure (FloatOp FAdd))
+            <|> try (string "-." >> pure (FloatOp FSub))
+            <|> try (string "+" >> pure (IntOp Add))
+            <|> try (string "-" >> pure (IntOp Sub))
         )
         <?> "a term operator"
 
 parseFactorOp :: Parser BinaryOp
 parseFactorOp =
     lexeme
-        ( try (string "*." >> return (FloatOp FMul))
-            <|> try (string "/." >> return (FloatOp FDiv))
-            <|> try (string "*" >> return (IntOp Mul))
-            <|> try (string "/" >> return (IntOp Div))
+        ( try (string "*." >> pure (FloatOp FMul))
+            <|> try (string "/." >> pure (FloatOp FDiv))
+            <|> try (string "*" >> pure (IntOp Mul))
+            <|> try (string "/" >> pure (IntOp Div))
         )
         <?> "a factor operator"
 
@@ -140,22 +142,24 @@ parsePattern =
     lexeme
         ( -- PRec
           try
-            ( parseKeyword "rec"
-                >> parseIdent
-                >>= \ident ->
-                    some parseIdent
-                        >>= \idents -> return (PRec ident idents)
+            ( do
+                parseKeyword "rec"
+                ident <- parseIdent
+                idents <- some parseIdent
+                pure (PRec ident idents)
             )
             -- PTuple
             <|> try
-                ( cSymbol '('
-                    >> sepBy1 parseIdent (cSymbol ',')
-                    >>= \idents -> cSymbol ')' >> return (PTuple idents)
+                ( do
+                    cSymbol '('
+                    idents <- sepBy1 parseIdent (cSymbol ',')
+                    cSymbol ')'
+                    pure (PTuple idents)
                 )
             -- PVar
             <|> try (parseIdent >>= \ident -> return (PVar ident))
         )
-        <?> "not a pattern"
+        <?> "a pattern"
 
 -- | Parses a simple expression
 parseSimpleExpr :: Parser ParsedExpr
@@ -168,16 +172,18 @@ parseSimpleExpr =
                 try (between (cSymbol '(') (cSymbol ')') parseExpr)
                     -- Tuple
                     <|> try
-                        ( between
-                            (cSymbol '(')
-                            (cSymbol ')')
-                            (sepBy1 parseExpr (cSymbol ','))
-                            >>= \exprs -> return (PGuard (Tuple pos exprs))
+                        ( do
+                            exprs <-
+                                between
+                                    (cSymbol '(')
+                                    (cSymbol ')')
+                                    (sepBy1 parseExpr (cSymbol ','))
+                            pure (PGuard (Tuple pos exprs))
                         )
                     -- Const
-                    <|> try (parseLiteral >>= \lit -> return (PGuard (Const pos lit)))
+                    <|> try (PGuard . Const pos <$> parseLiteral)
                     -- Var
-                    <|> try (parseIdent >>= \ident -> return (PGuard (Var pos ident)))
+                    <|> try (PGuard . Var pos <$> parseIdent)
         -- Remaining tokens can be components of Get.
         parseSimpleExpr' expr
     )
@@ -190,11 +196,11 @@ parseSimpleExpr =
         -- Get
         lexeme $
             try
-                ( getSourcePos
-                    >>= \pos ->
-                        cSymbol '.'
-                            >> between (cSymbol '(') (cSymbol ')') parseExpr
-                            >>= \expr2 -> parseSimpleExpr' (PGuard (Get pos expr1 expr2))
+                ( do
+                    pos <- getSourcePos
+                    cSymbol '.'
+                    expr2 <- between (cSymbol '(') (cSymbol ')') parseExpr
+                    parseSimpleExpr' (PGuard (Get pos expr1 expr2))
                 )
                 -- ... or None
                 <|> return expr1
@@ -203,169 +209,141 @@ parseSimpleExpr =
 parseExpr :: Parser ParsedExpr
 parseExpr =
     do
+        spaced
         parseExprWithPrecedence 9
         <?> "an expression"
   where
     parseExprWithPrecedence :: Int -> Parser ParsedExpr
     parseExprWithPrecedence precedence
+        | precedence > 0 =
+            lexeme $ try (parseExprWithPrecedence' precedence) <|> parseExprWithPrecedence (precedence - 1)
+        | otherwise =
+            lexeme $ try (parseExprWithPrecedence' precedence)
+
+    parseExprWithPrecedence' :: Int -> Parser ParsedExpr
+    parseExprWithPrecedence' precedence
         | precedence > 9 = error "Invalid precedence"
-        | precedence == 9 =
+        | precedence == 9 = do
             -- Let
-            lexeme $
-                try
-                    ( getSourcePos >>= \pos ->
-                        parseKeyword "let"
-                            >> parsePattern
-                            >>= \pat ->
-                                cSymbol '='
-                                    >> parseExpr
-                                    >>= \value ->
-                                        parseKeyword "in"
-                                            >> parseExprWithPrecedence 9
-                                            >>= \expr -> return (PGuard (Let pos pat (pExp value) (pExp expr)))
-                    )
-                    <|> parseExprWithPrecedence 8
-        | precedence == 8 =
+            pos <- getSourcePos
+            parseKeyword "let"
+            pat <- parsePattern
+            cSymbol '='
+            value <- parseExpr
+            parseKeyword "in"
+            expr <- parseExprWithPrecedence 9
+            pure (PGuard (Let pos pat (pExp value) (pExp expr)))
+        | precedence == 8 = do
             -- Then
-            lexeme $
-                try
-                    ( getSourcePos >>= \pos ->
-                        makeExprParser
-                            (parseExprWithPrecedence 7)
-                            [
-                                [ InfixR
-                                    ( cSymbol ';'
-                                        >> return (\left right -> PGuard (Let pos PUnit (pExp left) (pExp right)))
-                                    )
-                                ]
-                            ]
-                    )
-                    <|> parseExprWithPrecedence 7
-        | precedence == 7 =
+            pos <- getSourcePos
+            makeExprParser
+                (parseExprWithPrecedence 7)
+                [
+                    [ InfixR
+                        ( cSymbol ';'
+                            >> return (\left right -> PGuard (Let pos PUnit (pExp left) (pExp right)))
+                        )
+                    ]
+                ]
+        | precedence == 7 = do
             -- If
-            lexeme $
-                try
-                    ( getSourcePos >>= \pos ->
-                        parseKeyword "if"
-                            >> parseExprWithPrecedence 6
-                            >>= \cond ->
-                                parseKeyword "then"
-                                    >> parseExprWithPrecedence 6
-                                    >>= \then' ->
-                                        parseKeyword "else"
-                                            >> parseExprWithPrecedence 6
-                                            >>= \else' -> return (PGuard (If pos cond (pExp then') (pExp else')))
-                    )
-                    <|> parseExprWithPrecedence 6
-        | precedence == 6 =
+            pos <- getSourcePos
+            parseKeyword "if"
+            cond <- parseExprWithPrecedence 6
+            parseKeyword "then"
+            then' <- parseExprWithPrecedence 6
+            parseKeyword "else"
+            else' <- parseExprWithPrecedence 6
+            pure (PGuard (If pos cond (pExp then') (pExp else')))
+        | precedence == 6 = do
             -- Put
-            lexeme $
-                try
-                    ( do
-                        pos <- getSourcePos
-                        left <- parseSimpleExpr
-                        case left of
-                            PGuard (Get _ a idx) -> do
-                                right <- symbol "<-" >> parseExprWithPrecedence 5
-                                return $ PGuard (Put pos a idx right)
-                            _ -> fail "not a Put expression"
-                    )
-                    <|> parseExprWithPrecedence 5
-        | precedence == 5 =
+            pos <- getSourcePos
+            left <- parseSimpleExpr
+            case left of
+                PGuard (Get _ a idx) -> do
+                    right <- symbol "<-" >> parseExprWithPrecedence 5
+                    return $ PGuard (Put pos a idx right)
+                _ -> fail "a Put expression"
+        | precedence == 5 = do
             -- RelationBinOp
-            lexeme $
-                try
-                    ( do
-                        pos <- getSourcePos
-                        makeExprParser
-                            (parseExprWithPrecedence 4)
-                            [
-                                [ InfixL
-                                    ( do
-                                        (flipped, op) <- parseRelationBinOp
-                                        if flipped
-                                            then
-                                                return (\left right -> PGuard (Binary pos op right left))
-                                            else
-                                                return (\left right -> PGuard (Binary pos op left right))
-                                    )
-                                ]
-                            ]
-                    )
-                    <|> parseExprWithPrecedence 4
-        | precedence == 4 =
+            pos <- getSourcePos
+            makeExprParser
+                (parseExprWithPrecedence 4)
+                [
+                    [ InfixL
+                        ( do
+                            (flipped, op) <- parseRelationBinOp
+                            if flipped
+                                then
+                                    return (\left right -> PGuard (Binary pos op right left))
+                                else
+                                    return (\left right -> PGuard (Binary pos op left right))
+                        )
+                    ]
+                ]
+        | precedence == 4 = do
             -- TermOp
-            lexeme $
-                try
-                    ( getSourcePos
-                        >>= \pos ->
-                            makeExprParser
-                                (parseExprWithPrecedence 3)
-                                [
-                                    [ InfixL
-                                        ( parseTermOp
-                                            >>= \op -> return (\left right -> PGuard (Binary pos op left right))
-                                        )
-                                    ]
-                                ]
-                    )
-                    <|> parseExprWithPrecedence 3
-        | precedence == 3 =
+            pos <- getSourcePos
+            makeExprParser
+                (parseExprWithPrecedence 3)
+                [
+                    [ InfixL
+                        ( parseTermOp
+                            >>= \op -> return (\left right -> PGuard (Binary pos op left right))
+                        )
+                    ]
+                ]
+        | precedence == 3 = do
             -- FactorOp
-            lexeme $
-                try
-                    ( getSourcePos
-                        >>= \pos ->
-                            makeExprParser
-                                (parseExprWithPrecedence 2)
-                                [
-                                    [ InfixL
-                                        ( parseFactorOp
-                                            >>= \op -> return (\left right -> PGuard (Binary pos op left right))
-                                        )
-                                    ]
-                                ]
+            pos <- getSourcePos
+            makeExprParser
+                (parseExprWithPrecedence 2)
+                [
+                    [ InfixL
+                        ( parseFactorOp
+                            >>= \op -> return (\left right -> PGuard (Binary pos op left right))
+                        )
+                    ]
+                ]
+        | precedence == 2 = do
+            pos <- getSourcePos
+            -- FNeg
+            try
+                ( do
+                    symbol "-."
+                    expr <- parseExprWithPrecedence 2
+                    pure $ PGuard (Unary pos FNeg expr)
+                )
+                -- Neg
+                <|> try
+                    ( do
+                        cSymbol '-'
+                        expr <- parseExprWithPrecedence 2
+                        pure $ PGuard (Unary pos Neg expr)
                     )
-                    <|> parseExprWithPrecedence 2
-        | precedence == 2 =
-            getSourcePos >>= \pos ->
-                -- FNeg
-                lexeme $
-                    try
-                        ( symbol "-."
-                            >> parseExprWithPrecedence 2
-                            >>= \expr -> return $ PGuard (Unary pos FNeg expr)
-                        )
-                        -- Neg
-                        <|> try
-                            ( cSymbol '-'
-                                >> parseExprWithPrecedence 2
-                                >>= \expr -> return $ PGuard (Unary pos Neg expr)
-                            )
-                        <|> parseExprWithPrecedence 1
-        | precedence == 1 =
-            getSourcePos >>= \pos ->
-                -- App
-                try
-                    ( parseSimpleExpr
-                        >>= \func ->
-                            some parseSimpleExpr
-                                >>= \exprs -> return $ PGuard (App pos func exprs)
+        | precedence == 1 = do
+            pos <- getSourcePos
+            -- App
+            try
+                ( do
+                    func <- parseSimpleExpr
+                    exprs <- some parseSimpleExpr
+                    pure $ PGuard (App pos func exprs)
+                )
+                -- ArrayCreate
+                <|> try
+                    ( do
+                        parseKeyword "Array.create"
+                        expr1 <- parseSimpleExpr
+                        expr2 <- parseExprWithPrecedence 1
+                        pure $ PGuard (ArrayCreate pos expr1 expr2)
                     )
-                    -- ArrayCreate
-                    <|> try
-                        ( parseKeyword "Array.create"
-                            >> parseSimpleExpr
-                            >>= \expr1 ->
-                                parseExprWithPrecedence 1
-                                    >>= \expr2 -> return $ PGuard (ArrayCreate pos expr1 expr2)
-                        )
-                    -- Not
-                    <|> try
-                        ( parseKeyword "not"
-                            >> parseExprWithPrecedence 1
-                            >>= \expr -> return $ PGuard (Unary pos Not expr)
-                        )
-                    <|> parseExprWithPrecedence 0
+                -- Not
+                <|> try
+                    ( do
+                        parseKeyword "not"
+                        expr <- parseExprWithPrecedence 1
+                        pure $ PGuard (Unary pos Not expr)
+                    )
         | precedence == 0 = parseSimpleExpr
         | otherwise = error "Invalid precedence"
