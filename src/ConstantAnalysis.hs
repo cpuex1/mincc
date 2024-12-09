42954a249e3dc02f27efb6b1e67f95d07d82b702
Akihisa Yagi <caphosra@gmail.com>
2024-12-09 20:18:18 +0900

    Visit function bodies while finding constants

--- src/ConstantAnalysis.hs
+++ src/ConstantAnalysis.hs
@@ -3,12 +3,15 @@
 module ConstantAnalysis (registerConstants) where
 
 import IdentAnalysis (IdentEnvT, IdentProp (constant), updateProp)
-import Syntax (Expr (Const, If, Let), KExpr, Pattern (PVar))
+import Syntax (Expr (Const, If, Let), KExpr, Pattern (PRec, PVar))
 
 -- | Registers variables that hold a constant value.
 registerConstants :: (Monad m) => KExpr -> IdentEnvT m ()
 registerConstants (Let _ (PVar v) (Const _ lit) body) = do
     updateProp v (\p -> p{constant = Just lit})
+    registerConstants body
+registerConstants (Let _ (PRec _ _) expr body) = do
+    registerConstants expr
     registerConstants body
 registerConstants (Let _ _ _ body) = registerConstants body
 registerConstants (If _ _ t f) = do
