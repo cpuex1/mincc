d11c6767586203aa3e654def1035e0751b9f9462
Akihisa Yagi <caphosra@gmail.com>
2025-02-13 20:31:42 +0900

    Substitute phi instructions if the previous blocks are merged

--- src/BackEnd/Optim/Merging.hs
+++ src/BackEnd/Optim/Merging.hs
@@ -1,11 +1,17 @@
+{-# LANGUAGE GADTs #-}
+
 module BackEnd.Optim.Merging (mergeBlocks) where
 
+import BackEnd.Analysis.CodeBlock (fillInPrevBlocks)
 import BackEnd.Optim.Common (BackEndOptimStateT)
-import CodeBlock (BlockGraph (blocks), CodeBlock (CodeBlock, blockName), Terminator (TJmp), lookupBlock)
+import CodeBlock (BlockGraph (blocks), CodeBlock (CodeBlock, blockInst, blockName), Terminator (TJmp), lookupBlock)
+import Data.Map (delete, insert, lookup)
+import IR (Inst (IPhi), InstLabel)
+import Prelude hiding (lookup)
 
 mergeBlocks :: (Monad m) => BlockGraph a -> BackEndOptimStateT m (BlockGraph a)
 mergeBlocks graph =
-    pure $ graph{blocks = mergeBlockLoop (blocks graph)}
+    pure $ fillInPrevBlocks graph{blocks = mergeBlockLoop (blocks graph)}
   where
     mergeBlockLoop :: [CodeBlock a] -> [CodeBlock a]
     mergeBlockLoop [] = []
@@ -14,9 +20,21 @@ mergeBlocks graph =
             Just (CodeBlock _ afterInst [_] term) ->
                 -- The next block has only one predecessor.
                 let newBlock = CodeBlock beforeLabel (beforeInst ++ afterInst) prev term
-                 in mergeBlockLoop (newBlock : removed)
+                 in mergeBlockLoop (newBlock : substituted)
             _ -> block : mergeBlockLoop remains
       where
         nextBlock = lookupBlock nextLabel remains
         removed = filter (\block' -> blockName block' /= nextLabel) remains
+        substituted =
+            map
+                ( \block' -> block'{blockInst = map (substPhi nextLabel beforeLabel) $ blockInst block'}
+                )
+                removed
+
+        substPhi :: InstLabel -> InstLabel -> Inst ty -> Inst ty
+        substPhi from to inst@(IPhi state dest srcs) =
+            case lookup from srcs of
+                Just reg -> IPhi state dest $ insert to reg $ delete from srcs
+                Nothing -> inst
+        substPhi _ _ inst = inst
     mergeBlockLoop (block : remains) = block : mergeBlockLoop remains
