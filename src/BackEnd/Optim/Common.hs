5b218b9b24ee6309a9d6766d275e23f5a3952ff1
Akihisa Yagi <caphosra@gmail.com>
2025-02-13 14:09:00 +0900

    Update removeUnreachable

--- src/BackEnd/Optim/Common.hs
+++ src/BackEnd/Optim/Common.hs
@@ -5,16 +5,19 @@
 module BackEnd.Optim.Common (
     mapPhi,
     insertPhi,
+    updatePhi,
     BackEndOptimContext (..),
     BackEndOptimStateT,
 ) where
 
 import BackEnd.BackendEnv (BackendStateT)
+import CodeBlock (CodeBlock (blockInst, prevBlocks))
 import Control.Monad.State (MonadState (put), StateT, runState)
-import Data.Map (Map, insert, singleton)
+import Data.Map (Map, fromList, insert, singleton, toAscList)
 import Data.Maybe (catMaybes)
-import IR (Inst (IPhi), InstKind (AllowPhi, InstStateTy), InstLabel)
-import Registers (RegType (RFloat, RInt), Register (Register))
+import qualified Data.Set as S
+import IR (Inst (IMov, IPhi), InstKind (AllowPhi, InstStateTy), InstLabel)
+import Registers (RegOrImm (Reg), RegType (RFloat, RInt), Register (Register))
 
 -- | Search for a phi instruction and apply a function to it if found.
 mapPhi ::
@@ -60,6 +63,31 @@ insertPhi newState dest fromLabel src block =
 
     -- Create a new phi instruction if it does not exist.
     newPhi = IPhi newState dest $ singleton fromLabel src
+
+-- | Update phi instructions with previous blocks.
+updatePhi :: CodeBlock ty -> CodeBlock ty
+updatePhi block = block{blockInst = map modifyPhi $ blockInst block}
+  where
+    prev = S.fromList $ prevBlocks block
+
+    modifyPhi :: Inst ty -> Inst ty
+    modifyPhi (IPhi state dest srcs) =
+        if length srcs' == 1
+            then
+                IMov
+                    state
+                    dest
+                    ( Reg
+                        ( case srcs' of
+                            [(_, src)] -> src
+                            _ -> error "The length of sources should be 1."
+                        )
+                    )
+            else
+                IPhi state dest (fromList srcs')
+      where
+        srcs' = filter (\(label, _) -> S.member label prev) $ toAscList srcs
+    modifyPhi expr = expr
 
 data BackEndOptimContext = BackEndOptimContext
     deriving (Show, Eq)
--- src/BackEnd/Optim/Unreachable.hs
+++ src/BackEnd/Optim/Unreachable.hs
@@ -1,17 +1,21 @@
 module BackEnd.Optim.Unreachable (removeUnreachable) where
 
-import BackEnd.Optim.Common (BackEndOptimStateT)
-import CodeBlock (BlockGraph (BlockGraph), CodeBlock (blockName, prevBlocks))
+import BackEnd.Analysis.CodeBlock (fillInPrevBlocks)
+import BackEnd.Optim.Common (BackEndOptimStateT, updatePhi)
+import CodeBlock (BlockGraph (BlockGraph, blocks), CodeBlock (blockName, prevBlocks))
 
 -- | Remove unreachable blocks.
 removeUnreachable :: (Monad m) => BlockGraph a -> BackEndOptimStateT m (BlockGraph a)
-removeUnreachable (BlockGraph blocks entryLabel) =
-    pure $
+removeUnreachable (BlockGraph blocks' entryLabel) =
+    pure $ filled{blocks = map updatePhi (blocks filled)}
+  where
+    graph =
         BlockGraph
             ( filter
                 ( \block ->
                     not (null $ prevBlocks block) || blockName block == entryLabel
                 )
-                blocks
+                blocks'
             )
             entryLabel
+    filled = fillInPrevBlocks graph
